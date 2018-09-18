121f52f6560079a02af9b2db98f27b90ca48be41
UCAS-BigBird <oy199603@qq.com>
2018-09-18 15:42:33 +0800

    再写一点

--- selectionsort.py
+++ selectionsort.py
@@ -1,1 +1,19 @@
-print('hello_world')
+def findSmallest(arr):
+    smallest=arr[0]
+    smallest_index=0
+    for i in range(1,len(arr)):
+        if arr[i]<smallest:
+            smallest=arr[i]
+            smallest_index=i
+    return smallest_index
+
+def selectionSort(arr):
+    newArr=[]
+    for i in range(len(arr)):
+        smallest=findSmallest(arr)
+        newArr.append(arr.pop(smallest))
+    return  newArr
+
+a=[7,4,5,9,2]
+a=selectionSort(a)
+print(a)
